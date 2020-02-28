9d619e1fe9ddbe87bd6a4aaac9e64504ea231891
Michael Hochpöchler <s1810307018@students.fh-ooe.at>
2020-02-28 18:05:23 +0100

    range check added

--- FibonacciSeries/Program.cs
+++ FibonacciSeries/Program.cs
@@ -12,13 +12,16 @@ namespace FibonacciSeries {
       Console.Out.WriteLine();
     }
 
-    static int CreateFibonacciNumber(int index) {
+    static int CreateFibonacciNumber(int index) {	  
       if (index == 0) return 0;
       if (index == 1) return 1;
       return CreateFibonacciNumber(index - 1) + CreateFibonacciNumber(index - 2);
     }
 
     static int[] CreateFibonacciSeries(int count) {
+		if (count < 0) {
+		  Console.Out.Write("Amount of Fibonacci Numbers has to be positive");
+	  }
       int[] fibonacciSeries = new int[count];
       for (int i = 0; i < count; i++)
         fibonacciSeries[i] = CreateFibonacciNumber(i);
